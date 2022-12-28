5f27def44459a6e299414fa6d0a4f7e01128addd
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-12-29 00:10:39 +0100

    end first ui edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,21 @@ import 'bootstrap/dist/js/bootstrap.js';
 import Navbar from './components/navar';
 import Footer from './components/footer';
 import Table from './components/table';
+import Login from './pages/Login'
+import Register from './pages/Register';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
-      <Table/>
+      <Routes>
+        <Route path="dashboard" element={<Table/>} />
+        <Route path="/" element={<Login/>} />
+        <Route path="/Register" element={<Register/>} />
+      </Routes>
       <Footer/>
-      
-
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,28 @@
-const Login = ()=>{
+const Register = ()=>{
     return(
         <section class="py-4">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-md-9 col-lg-6 col-xl-5">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-Register-form/draw2.webp"
           class="img-fluid" alt="Sample image"/>
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <form>
-               
+
+          <div class="form-outline mb-4">
+            <input type="email" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Enter a valid fullname please" />
+            <label class="form-label" for="form3Example3">fullname</label>
+          </div>
+
+
           <div class="form-outline mb-4">
             <input type="email" id="form3Example3" class="form-control form-control-lg"
               placeholder="Enter a valid email address" />
             <label class="form-label" for="form3Example3">Email address</label>
           </div>
+       
 
          
           <div class="form-outline mb-3">
@@ -31,14 +39,13 @@ const Login = ()=>{
                 Remember me
               </label>
             </div>
-            <a href="#!" class="text-body">Forgot password?</a>
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
             <button type="button" class="btn btn-primary btn-lg"
-              >Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                class="link-danger">Register</a></p>
+              >Register</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">you have an account? <a href="#!"
+                class="link-danger">Login</a></p>
           </div>
 
         </form>
@@ -49,4 +56,4 @@ const Login = ()=>{
     )
 }
 
-export default Login;
+export default Register;
